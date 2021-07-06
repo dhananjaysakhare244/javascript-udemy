e4915c61f01de481128e32aa75a455cb5e16d990
Dhananjay Sakhare <dhananjaysakhare244@gmail.com>
2021-07-06 20:53:51 +0530

    Adding syntax for multiline strings

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//multi line strings
+
+const multiLine = "This is \n\
+multiple line \n\
+string.";
+
+console.log(multiLine);
+
+const multiLineNew = `This is
+multiple line with
+string template`;
+
+console.log(multiLineNew);
